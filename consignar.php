5e9706779125c8265fe70e8a8e4f13b56a1c7614
juanAngaritaweb <juanAngaritaweb@hotmail.com>
2020-11-25 13:40:57 -0500

    list consignar

--- consignar.php
+++ consignar.php
@@ -5,6 +5,48 @@
     </head>
     <body>
         <h1>Consignar</h1>
+        <form action="consignar_post.php" method="post">
+            <label for="accion">Transferir a</label>
+            <select name ="accion" id="accion">
+                <option
+                    value="Cuenta"
+                >Cuenta
+                </option>
+                <option
+                        value="Credito"
+                >Credito
+                </option>
+            </select>
+            <br>
+            <label for="IDP">ID</label><br>
+            <input type="text" name="IDP"><br>
+            <label for="cantidad">Cantidad</label><br>
+            <input type="text" name="cantidad">
+            <select name ="tipoMoneda" id="tipoMoneda">
+                <option
+                        value="JaveCoin"
+                >JaveCoin
+                </option>
+                <option
+                        value="COP"
+                >COP
+                </option>
+            </select><br>
+
+            <?php
+                session_start();
+                if(isset($_SESSION['IDUsuario'])){
+                }
+                else{
+                    echo "<label for=\"cedula\">Cedula</label><br>";
+                    echo "<input type=\"text\" name=\"cedula\"><br>";
+                }
+            ?>
+
+            <input type="submit" value="Consignar" name="SubmitButton">
+
+        </form>
+
         <input type='button'value='Regresar al index' onclick="document.location.href='index.php';"/>
     </body>
 </html>
---
+++ consignar_post.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <!-- Imprimir resultado de la transaccion -->
+    <?php
+    $errRetiro = "";
+
+    $accion = $_POST["accion"];
+    $IDP =  $_POST["IDP"];
+    $cantidad = $_POST["cantidad"];
+    $cedula = $_POST["cedula"];
+    $tipoMoneda = $_POST["tipoMoneda"];
+
+    if($tipoMoneda == "COP"){
+        $cantidad = $cantidad/1000;
+    }
+
+    session_start();
+
+    include_once dirname(__FILE__) . '/config.php';
+    $con = mysqli_connect(HOST_DB, USUARIO_DB, USUARIO_PASS, NOMBRE_DB);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (empty($_POST["cantidad"]) or empty($_POST["IDP"]) or empty($_POST["cantidad"])){
+            $errRetiro = "Transaccion declinada: Llene todos los campos";
+        }
+        else if(empty($_POST["cedula"]) and !isset($_SESSION['IDUsuario'])){
+            $errRetiro = "Transaccion declinada: Es necesario la cedula";
+        }
+
+        else if (!preg_match("/^[0-9]*$/",$cantidad))
+        {
+            $errRetiro = "Transaccion declinada: Ingrese una cantidad valida";
+        }
+        else if (!preg_match("/^[0-9]*$/",$IDP))
+        {
+            $errRetiro = "Transaccion declinada: Ingrese una cuenta valida";
+        }
+        else if (mysqli_connect_errno()) {
+            $errRetiro = "Error en la conexión: ";
+        }else {
+
+            if($accion == "Cuenta"){
+
+                $sql = "UPDATE `cuenta` 
+                SET Saldo = Saldo + $cantidad
+                WHERE `PID` = $IDP";
+
+                $resultado = mysqli_query($con,$sql );
+                if(mysqli_affected_rows($con) !== 0){
+                    $errRetiro = "Transaccion exitosa";
+                }else{
+                    $errRetiro = "Cuenta inexistente";
+                }
+            }else{
+
+                $sql = "SELECT * FROM `credito` WHERE `PID` = $IDP";
+                $existeCuenta = mysqli_query($con,$sql);
+                $row = mysqli_fetch_row($existeCuenta);
+                if(implode(null,$row) == null){ $errRetiro = "El credito a transferir no existe";}
+                else if( ( $row[2] - $cantidad ) < 0 ) {
+                    $errRetiro = "Maxima cantidad a consignar: $row[2] JaveCoins";
+                }else{
+                    $sql = "UPDATE `credito` 
+                     SET Saldo = CASE
+                        WHEN (Saldo-$cantidad)>=0 THEN Saldo-$cantidad
+                        ELSE Saldo
+                     END  WHERE `PID` = $IDP;";
+                    $resultado = mysqli_query($con,$sql);
+                    $resta = $row[2] - $cantidad;
+                    $errRetiro = "Transaccion exitosa. Credito restante:$resta";
+                }
+            }
+        }
+
+
+    }
+    ?>
+
+    <!-- Alerta con el resultado de la transaccion -->
+    <?php echo $errRetiro;
+    echo "<script>
+                alert(\"$errRetiro\");
+                window.location.href = \"index.php\";
+            </script>"
+    ?>
+</body>
+</html>
