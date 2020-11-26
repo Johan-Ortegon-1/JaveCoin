300b1ce877b65d9371aadaacb3fae47e43493bf5
Santiago Palacios <spalaciosl1007@gmail.com>
2020-11-26 15:47:46 -0500

    Prueba push

--- ver_mis_productos.php
+++ ver_mis_productos.php
@@ -4,7 +4,7 @@
         <title>Ver mis productos</title>
     </head>
     <body>
-        <h1>Ver mis productos</h1>
+        <h1>Ver mis productoos</h1>
         <input type='button'value='Regresar al index' onclick="document.location.href='index.php';"/>
     </body>
 </html>
